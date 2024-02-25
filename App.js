fc840fc58de266aeb3f5c532fd154d3f20a5f99b
awwnonya <palananya08@gmail.com>
2024-02-26 00:57:31 +0530

    header button added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
@@ -36,7 +36,11 @@ export default function App() {
             //   };
             // }}
           />
-          <Stack.Screen name='Meal Detail' component={MealDetailScreen}/>
+          <Stack.Screen name='Meal Detail' component={MealDetailScreen} options={{
+            headerRight: () => {
+              return <Button title='Tap me!'/>
+            }
+          }}/>
 
         </Stack.Navigator>
       </NavigationContainer>
