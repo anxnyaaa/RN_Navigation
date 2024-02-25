26fa1c061b66de32711a48bedde7e99571a07287
awwnonya <palananya08@gmail.com>
2024-02-25 23:20:43 +0530

    navigation and category details added

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,45 @@
-import { View, Text, StyleSheet } from 'react-native';
-import {MEALS} from '../data/dummy-data' 
+import { useLayoutEffect } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import {MEALS, CATEGORIES} from '../data/dummy-data' 
+import MealItem from '../components/MealItem';
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({route, navigation}) {
+    const catId = route.params.categoryId;
+
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >=0;
+    });
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find(
+            (category) => category.id === catId
+        ).title;
+
+        navigation.setOptions({title: categoryTitle});
+    }, [catId, navigation]);
+
+    function renderMealItem(itemData) {
+        const item= itemData.item;
+        const mealItemProps = {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            affordability: item.affordability,
+            complexity: item.complexity,
+            duration: item.duration,
+
+        };
+        return (
+            <MealItem {...mealItemProps}/>
+        );
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen</Text>
+            {/* <Text style={{color: '#5cba63'}}>Meals Overview Screen - {catId}</Text> */}
+            <FlatList 
+                data={displayedMeals} 
+                keyExtractor={(item) => item.id} 
+                renderItem={renderMealItem}/>
         </View>
     )
 };
